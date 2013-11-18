779de17f7b5ec69f7ea850084adefba345b0b5d8
Xavier <xavier@madewithlove.be>
2013-11-18 02:40:14 +0000

    Added check to verify if the head plugin for elastic search was already installed.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 puppet-elasticsearch
 ====================
+
+###Installation
+
+Just add the following code to your default.pp file.
+
+	include elasticsearch
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,31 @@
 class elasticsearch {
 
-	# If it doesnt work, manually do sudo apt-get -f install
-	if ! defined(Package['openjdk-6-jre']) {
-		package { ["openjdk-6-jre"]:
-				ensure => installed,
-				require => Class['apt']
-		}
-	}
-	
-	exec { 'wget elasticsearch':
-				command => "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb -O elasticsearch.deb",
-				unless => "ls | grep elasticsearch.deb"
-			}
+  # Checks if the package was allready defined
+  if ! defined(Package['openjdk-6-jre']) {
+    package { ["openjdk-6-jre"]:
+        ensure => installed,
+        require => Class['apt']
+    }
+  }
+  
+  # Downloads elasticsearch
+  exec { 'wget elasticsearch':
+        command => "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb -O elasticsearch.deb",
+        unless => "ls | grep elasticsearch.deb"
+      }
+  
+  # Installs elastic search
+  exec { 'dpkg -i elasticsearch.deb':
+    require => [Package['openjdk-6-jre'], Exec['wget elasticsearch']],
+    notify => Service["elasticsearch"]
+  }
 
-	exec { 'dpkg -i elasticsearch.deb':
-		require => [Package['openjdk-6-jre'], Exec['wget elasticsearch']],
-		notify => Service["elasticsearch"]
-	}
+  service { elasticsearch : ensure => running }
 
-	service { elasticsearch : ensure => running }
-
-	exec { 'sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head':
-  	
-	}
+  # Installs elastic search head plugin and checks if it was already installed
+  exec { 'install elasticsearch plugins':
+  	command => "sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head",
+  	require => [Exec['dpkg -i elasticsearch.deb']],
+	unless => "sudo /usr/share/elasticsearch/bin/plugin -l | grep head"
+  }
 }
